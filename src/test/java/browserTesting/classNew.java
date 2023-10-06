572c049ee45eb638181fa8232adf5a3b2806ad49
mdsharifuddin <mduddin1924@gmail.com>
2023-10-06 04:04:15 -0400

    New class andd three line added

---
+++ src/test/java/browserTesting/classNew.java
@@ -0,0 +1,9 @@
+package browserTesting;
+
+public class classNew {
+    public static void main(String[] args) {
+        System.out.println("new class added and one line");
+        System.out.println("I lost everything from this project");
+        System.out.println("Finally resolved big problem");
+    }
+}
