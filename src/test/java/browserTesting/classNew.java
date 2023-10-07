97572110dcd8c4c1d3295716f42baede5fca6683
mdsharifuddin <mduddin1924@gmail.com>
2023-10-07 02:51:02 -0400

    two line added

--- src/test/java/browserTesting/classNew.java
+++ src/test/java/browserTesting/classNew.java
@@ -5,5 +5,7 @@ public class classNew {
         System.out.println("new class added and one line");
         System.out.println("I lost everything from this project");
         System.out.println("Finally resolved big problem");
+        System.out.println("Bismillahir Rahmanir Rahim");
+        System.out.println("Allah tumi sobaire hedayet dan koro");
     }
 }
