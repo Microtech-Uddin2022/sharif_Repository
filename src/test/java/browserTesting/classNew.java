35ac3d24bd3ae5bd421741cdb25d5a83b8c719cc
mdsharifuddin <mduddin1924@gmail.com>
2023-10-08 22:59:02 -0400

    conflict

--- src/test/java/browserTesting/classNew.java
+++ src/test/java/browserTesting/classNew.java
@@ -3,7 +3,7 @@ package browserTesting;
 public class classNew {
     public static void main(String[] args) {
         System.out.println("new class added and one line");
-        System.out.println("I lost everything from this project");
+
         System.out.println("Finally resolved big problem");
         System.out.println("4th line for branches new class added and one line");
         System.out.println("I lost everything from this project forever");
